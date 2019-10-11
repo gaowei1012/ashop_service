3b06f6ce7a5c5edf182b1f82f4c6574b4f2568c5
gmw-zjw <mingwei2222@126.com>
2019-10-11 17:21:52 +0800

    添加路由配置项

--- app/router/router.local.js
+++ app/router/router.local.js
@@ -4,7 +4,6 @@ module.exports = app => {
   // 基础接口
   app.get('/api/home/index', 'home.home.index')
   app.get('/api/home/banner', 'home.banner.index')
-  app.get('/api/shop/all', 'shop.all.index')
   app.post('/api/user/login', app.jwt, 'user.login.login')
   
   // 支付接口
--- app/router/router.prod.js
+++ app/router/router.prod.js
@@ -4,7 +4,9 @@ module.exports = app => {
 
   // 基础接口
   app.get('/api/shop/all', app.controller.shop.all.index)
+  app.post('/api/user/login', app.jwt, 'user.login.login')
 
   // 支付接口
   
+  // 微信小程序接口
 }
