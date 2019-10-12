501e50b67da4bc9f1c31eaac707ee146fb42dd73
gmw-zjw <mingwei2222@126.com>
2019-10-12 15:18:49 +0800

    del users

--- app/service/users.js
+++ app/service/users.js
@@ -1,15 +1,1 @@
-'use strict'
 
-const Service = require('egg').Service
-
-class UsersService extends Service {
-  async login() {
-    const { ctx } = this
-    // todo
-  }
-  async getInfo() {
-    const { ctx } = this
-  }
-}
-
-module.exports = UsersService
