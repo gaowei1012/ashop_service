a1d5223a0ba24e9d98967ea187c948dd8764037b
gmw-zjw <mingwei2222@126.com>
2019-10-12 09:49:53 +0800

    添加接口版本

--- app/router/router.local.js
+++ app/router/router.local.js
@@ -2,9 +2,9 @@
 module.exports = app => {
 
   // 基础接口
-  app.get('/api/home/index', 'home.home.index')
-  app.get('/api/home/banner', 'home.banner.index')
-  app.post('/api/user/login', app.jwt, 'user.login.login')
+  app.get('/api/v1/home/index', 'home.home.index')
+  app.get('/api/v1/home/banner', 'home.banner.index')
+  app.post('/api/v1/user/login', app.jwt, 'user.login.login')
   
   // 支付接口
 
