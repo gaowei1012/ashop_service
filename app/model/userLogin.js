99d7c7b7c77801a7cc5a908add79167dbfc38737
gmw-zjw <mingwei2222@126.com>
2019-10-13 20:27:23 +0800

    +

--- app/model/userLogin.js
+++ app/model/userLogin.js
@@ -3,17 +3,17 @@
 const bcryptjs = require('bcryptjs')
 
 module.exports = app => {
-  let userLogin = app.model.import('../domain/userLogin')
+  // let userLogin = app.model.import('../domain/userLogin')
 
-  userLogin.updatedPassword = async function(username, pass) {
-    let salt = bcryptjs.genSaltSync(10)
-    let npass = bcryptjs.hashSync(pass,salt)
-    const userLogin = await app.findOne({where: {username: username}})
-    if (!userLogin) {
-      return null
-    }
-    userLogin.update({npass, salt})
-    return userLogin
-  }
-  return userLogin
+  // userLogin.updatedPassword = async function(username, pass) {
+  //   let salt = bcryptjs.genSaltSync(10)
+  //   let npass = bcryptjs.hashSync(pass,salt)
+  //   const userLogin = await app.findOne({where: {username: username}})
+  //   if (!userLogin) {
+  //     return null
+  //   }
+  //   userLogin.update({npass, salt})
+  //   return userLogin
+  // }
+  // return userLogin
 }
--- app/router.js
+++ app/router.js
@@ -5,11 +5,11 @@ module.exports = app => {
   /**
    * 根据环境加载路由
    */
-  // if (app.config.env === 'prod') {
-  //   require('./router/router.prod')(app)
-  // } else {
-  //   require('./router/router.local')(app)
-  // }
+  if (app.config.env === 'prod') {
+    require('./router/router.prod')(app)
+  } else {
+    require('./router/router.local')(app)
+  }
   // 指定本地开发环境
-  require('./router/router.local')(app)
+  // require('./router/router.local')(app)
 }
